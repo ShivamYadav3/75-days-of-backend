187e54ed315ba2931be1e1b2a57c2262d635c0e0
Shivam Yadav <shivamyadav93000@gmail.com>
2024-03-19 00:00:42 +0530

    routes

--- day-2/index.js
+++ day-2/index.js
@@ -1,16 +1,28 @@
 const http = require("node:http");
 const hostname = "localhost"; // loopback ip or localhost
 const port = 8000; // OS - 65500
+const url = require("url");
+const { data } = require("./data");
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
+  const q = url.parse(req.url, true).query;
+  // console.log(req.url, q);
   res.setHeader("Content-Type", "application/json");
-  const payload = {
-    name: "shivam",
-    city: "jabalpur",
-    Lname: "yadav",
-  };
-  res.end(JSON.stringify(payload));
+  // const payload = {
+  //   name: "shivam",
+  //   city: "jabalpur",
+  //   Lname: "yadav",
+  // };
+  if (req.url == "/greetings") {
+    res.end("hello there");
+  } else if (req.url == "/products") {
+    res.end(JSON.stringify(data));
+  } else if (q.name) {
+    res.end(`hello ${q.name}`);
+  } else {
+    res.end("Welcome to Card book");
+  }
 });
 
 server.listen(port, hostname, () => {
